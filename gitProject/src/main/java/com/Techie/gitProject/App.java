31e6a5bd5dd9a5e905bbe5569652585f182f34a9
Sangeetha229 <89497314+Sangeetha229@users.noreply.github.com>
2026-02-14 21:51:55 -0600

    changes made by Developer2 in App.java

--- gitProject/src/main/java/com/Techie/gitProject/App.java
+++ gitProject/src/main/java/com/Techie/gitProject/App.java
@@ -5,6 +5,7 @@ package com.Techie.gitProject;
  */
 public class App {
     public static void main(String[] args) {
+        System.out.println("Changes done by Developer2");
         System.out.println("Hello World changed!");
     }
 }
