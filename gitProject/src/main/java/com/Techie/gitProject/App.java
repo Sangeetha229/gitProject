8706b625d0bf4647f808523016c5b295c83d7aa0
s_e229 <s_e229@txstate.edu>
2026-02-15 17:30:05 -0600

    exception

--- gitProject/src/main/java/com/Techie/gitProject/App.java
+++ gitProject/src/main/java/com/Techie/gitProject/App.java
@@ -8,19 +8,20 @@ public class App {
 	 public static void fibonacci(int n) {
 			int a = 0, b = 1;
 			System.out.print("Fibonacci Series: ");
-			for (int i = 0; i < n; i++) {
-				System.out.print(a + " ");
-				int next = a + b;
-				a = b;
-				b = next;
-			}
+//			for (int i = 0; i < n; i++) {
+				System.out.print(b/a + " ");
+//				int next = a + b;
+//				a = b;
+//				b = next;
+//			}
 			System.out.println();
 		}
 	
-    public static void main(String[] args) {
-        System.out.println("Changes done by Developer2");
-        System.out.println(" feature changes!");
-    }
-    //add a new method to calculate fibonacci series 
-   
+   public static void main(String[] args) {
+       System.out.println("Changes done by Developer2");
+       System.out.println(" feature changes!");
+       fibonacci(1);
+        }
+   //add a new method to calculate fibonacci series 
+  
 }
